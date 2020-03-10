4d620e2605cc054e5d27e1c00b3aa4366d4965d0
liyingxiu <yingxiuim@gmail.com>
2020-03-10 21:34:21 +0800

    设置下拉菜单，退出

--- node-app/client/src/store/index.js
+++ node-app/client/src/store/index.js
@@ -3,48 +3,55 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 const types = {
-  SET_AUTHENTICATED:"SET_AUTHENTICATED",
-  SET_USER:'SET_USER'
+  SET_AUTHENTICATED: "SET_AUTHENTICATED",
+  SET_USER: 'SET_USER'
 };
 
 //状态信息
 const state = {
-    isAuthenticated:false,
-    user:{}
+  isAuthenticated: false,
+  user: {}
 };
 
 //获取状态信息
 const getters = {
-  isAuthenticated:state=>state.isAuthenticated,
-  user:state=>state.user
+  isAuthenticated: state => state.isAuthenticated,
+  user: state => state.user
 }
 
 //更改对应的状态信息
 const mutations = {
   //设置是否授权的方法
-  [types.SET_AUTHENTICATED](state,isAuthenticated){
-    if(isAuthenticated) state.isAuthenticated = isAuthenticated;
+  [types.SET_AUTHENTICATED](state, isAuthenticated) {
+    if (isAuthenticated) state.isAuthenticated = isAuthenticated;
     else state.isAuthenticated = false;
   },
-  [types.SET_USER](state,user){
-    if(user) state.user = user;
+  [types.SET_USER](state, user) {
+    if (user) state.user = user;
     else state.user = {};
   }
 }
 
 //异步操作，即调用mutations
 const actions = {
-  setAuthenticated:({commit},isAuthenticated) => {
+  //授权
+  setAuthenticated: ({ commit }, isAuthenticated) => {
     commit(types.SET_AUTHENTICATED, isAuthenticated)
   },
-  setUser:({commit},user)=>{
-    commit(types.SET_USER,user)
+  //用户
+  setUser: ({ commit }, user) => {
+    commit(types.SET_USER, user)
+  },
+  //清空当前状态，退出
+  clearCurrentState: ({ commit }) => {
+    commit(types.SET_AUTHENTICATED, false);
+    commit(types.SET_USER, null);
   }
 }
 
 export default new Vuex.Store({
-state,
-getters,
-mutations,
-actions
+  state,
+  getters,
+  mutations,
+  actions
 })
